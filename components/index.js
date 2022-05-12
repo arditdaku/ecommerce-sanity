3db6fc8451fd4fc0ddcac53c0effda8dfe26d62a
Ardit Daku <arditdaku1@gmail.com>
2022-05-12 22:56:50 +0200

    Setup application, sanity & components & hero banner

---
+++ components/index.js
@@ -0,0 +1,6 @@
+// export { default as Footer } from "./Footer";
+// export { default as Layout } from "./Layout";
+// export { default as Nav } from "./Navbar";
+// export { default as Prodcut } from "./Product";
+// export { default as HeroBanner } from "./HeroBanner";
+// export { default as Cart } from "./Cart";
